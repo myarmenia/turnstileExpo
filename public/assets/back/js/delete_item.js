6920ff52e4bef329c5c9286335bc7b9baf49e995
NarineHay <naromisho87@gmail.com>
2023-05-05 15:04:50 +0400

    NH_05.05.23_15:04

---
+++ public/assets/back/js/delete_item.js
@@ -0,0 +1,28 @@
+deleteItem()
+
+function deleteItem(){
+    document.querySelectorAll('.delete_item').forEach(el=>{
+        el.addEventListener('click', deleteItemFromDB)
+    })
+}
+
+function deleteItemFromDB()
+{
+
+    let item_id = this.getAttribute('data-id')
+    let type = this.getAttribute('data-type')
+    let table = this.getAttribute('data-table')
+    let that = this
+    if(document.querySelectorAll('.'+type).length > 1){
+        csrf = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+        fetch('/delete_item/'+item_id+'/'+table+'/'+type,{
+            method: 'get',
+            headers: {'X-CSRF-TOKEN':csrf },
+        }).then(async response => {
+                if (response.ok) {
+                    that.parentNode.remove()
+                }
+            })
+    }
+}
