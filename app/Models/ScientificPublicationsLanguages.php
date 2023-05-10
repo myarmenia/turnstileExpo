5076c486cd8c77658aed72eccdf435bed6cfc5d2
KirakosyanSuren <kirakosyan.suren@inbox.ru>
2023-05-10 17:37:15 +0400

    10.05.2023 commit-2

--- app/Models/ScientificPublicationsLanguages.php
+++ app/Models/ScientificPublicationsLanguages.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class ScientificPublicationsLanguages extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'sc_publication_id',
+        'language_id',
+        'content',
+    ];
+
+    public function scientific_publication()
+    {
+        return $this->belongsTo(ScientificPublications::class, 'sc_publication_id');
+    }
 }
