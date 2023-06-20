57abf106bbf0fddede165bfd967c0ba3ebc9fa01
NarineHay <naromisho87@gmail.com>
2023-06-20 14:18:22 +0400

    NH_20.06.23_14:18

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -44,10 +44,10 @@ window.Echo = new Echo({
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     // encrypted: true,
     encrypted: false,
-    forceTLS: true,
+    forceTLS: false,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    wssPort: 6001,
+    // wssPort: 6001,
     enabledTransports: ['ws', 'wss'],
 });
 
