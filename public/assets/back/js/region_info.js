88c8dc8f7001ed95cdf6a0145e393f53c6afc1d9
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2023-06-27 14:30:06 +0400

    AK27.06.23:14:30

--- public/assets/back/js/region_info.js
+++ public/assets/back/js/region_info.js
@@ -12,26 +12,7 @@ image_path.addEventListener("change", (e) => {
     document.getElementById('image_path_error').innerHTML=''
 
 })
-schema_path.addEventListener("change", (e) => {
 
-    document.querySelector('.schema_path_div').innerHTML=''
-
-    let file = e.target.files;
-    let url = URL.createObjectURL(file[0])
-
-    document.querySelector('.schema_path_div').innerHTML = `<div class="d-flex file_div">
-                                                        <img src="${url}">
-
-                                                    </div>`;
-
-    let errorSchemaDiv=document.getElementById('schema_path_error')
-
-    if(errorSchemaDiv!=null){
-
-        document.getElementById('schema_path_error').innerHTML=''
-    }
-
-})
 region_info_files.addEventListener("change", (e) => {
 
 
@@ -71,7 +52,7 @@ region_info_files.addEventListener("change", (e) => {
 
         document.getElementById('region_info_files_error').innerHTML=''
     }
-   
+
 
 })
 
