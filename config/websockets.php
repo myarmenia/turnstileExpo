3fa288d8eadf87010c6a50044089732668e3279c
NarineHay <naromisho87@gmail.com>
2024-10-01 11:13:40 +0400

    01.10-v1

--- config/websockets.php
+++ config/websockets.php
@@ -132,7 +132,7 @@ return [
          */
         // 'passphrase' => env('LARAVEL_WEBSOCKETS_SSL_PASSPHRASE', null),
         'passphrase' => null,
-        'verify_peer' => false,
+        'verify_peer' => true,
 
     ],
 
