5a516ce1d23044b617ed6f26be2606e8f5a9e64e
NarineHay <naromisho87@gmail.com>
2024-10-01 12:40:08 +0400

    01.10-v7

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -52,6 +52,10 @@ return [
                 'scheme' => 'https'
 
             ],
+            'curl_options' => [
+                CURLOPT_SSL_VERIFYHOST => 0,
+                CURLOPT_SSL_VERIFYPEER => 0,
+            ]
             // 'client_options' => [
             //     // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             // ],
